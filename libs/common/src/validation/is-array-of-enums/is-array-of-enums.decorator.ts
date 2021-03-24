e1786d22bba38b2534f926eee34ba0cef515aace
Olexiy Ivashkin <ivashkinog@gmail.com>
2021-03-24 12:43:14 +0200

    fix(validation): fix array of enums validation decorator

--- libs/common/src/validation/is-array-of-enums/is-array-of-enums.decorator.ts
+++ libs/common/src/validation/is-array-of-enums/is-array-of-enums.decorator.ts
@@ -4,28 +4,27 @@ import { ArrayMaxSize, IsIn, IsOptional } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 import { NOOP } from '../../utils';
-import { EnumValues } from 'enum-values';
 
 export interface IsArrayOfEnumsOptions {
-    enum: any;
+    enum: string[];
     isOptional?: boolean;
     apiDescription?: string;
 }
 
 export const IsArrayOfEnums = (options: IsArrayOfEnumsOptions) => {
     const { isOptional = false, apiDescription } = options;
-    const enumValues = EnumValues.getValues(options.enum);
 
     return applyDecorators(
         ApiProperty({
             type: [String],
             format: 'form',
             required: !isOptional,
+            enum: options.enum || [],
             description: apiDescription ?? 'Comma separated enum values',
         }),
         isOptional ? IsOptional() : NOOP,
-        ArrayMaxSize(enumValues.length),
-        IsIn(EnumValues.getValues(options.enum), { each: true }),
-        Transform((value: string) => (!!value ? value.split(',') : null)),
+        ArrayMaxSize(options.enum.length),
+        IsIn(options.enum, { each: true }),
+        Transform((value: string) => (!!value ? value.split(',').map(_ => _.trim()) : null)),
     );
 };
